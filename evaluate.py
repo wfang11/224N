056a241dba5508f6498871e196f4b8f7471cb2ac
William Fang <62477806+wfang11@users.noreply.github.com>
2024-05-20 01:20:55 -0700

    add evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -2,11 +2,9 @@ from models.model import Model
 from data.cai import CAI
 
 def __main__(): 
-    model = Model("GPT-4")
-    print(model.query())
-    # cai = CAI()
-    # data = cai.get_data()
-    ### TODO: set up a pipeline for running inference and then evaluating 
+    for name in "LLAMA", "GPT-4", "GEMINI": 
+        model = Model(name)
+        print(model.query())
 
 if __name__ == "__main__": 
     __main__()
