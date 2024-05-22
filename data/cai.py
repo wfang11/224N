08ec6657eee377b36f3bdc5cd530bcfe505ffc98
Vikram Sivashankar <vikram29@stanford.edu>
2024-05-21 23:15:59 -0700

    Added extract_real_principles to cai and also a test script to see real principles

--- data/cai.py
+++ data/cai.py
@@ -3,3 +3,7 @@ from datasets import load_dataset
 class CAI: 
     def __init__(self, dataset="train_sft"): 
         self.dataset = load_dataset("HuggingFaceH4/cai-conversation-harmless")[dataset]
+
+    def extract_real_principles(self):
+        return [dp['revision_prompt'] for dp in self.dataset]
+
