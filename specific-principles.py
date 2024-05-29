e539c4d159d3a6e22ea702489bf5ee8a99a5614c
William Fang <62477806+wfang11@users.noreply.github.com>
2024-05-29 01:44:12 -0700

    tails 0-9

--- specific-principles.py
+++ specific-principles.py
@@ -24,28 +24,43 @@ def generate_principles(subset):
 
 
 def __main__(): 
+    START = 20
+    END = 200
 
-    START = 10
-    END = 20
-    subset = [y for x, y in enumerate(data_points) if START <= x < END] if DEBUG else data_points # weird hack to slice the dataset 
+    errored = [34, 46, 52, 82, 93, 95, 103, 109, 110, 126, 146, 159, 175, 176, 179, 193, 195]
+    subset = [y for x, y in enumerate(data_points) if x in errored]
+    # subset = [y for x, y in enumerate(data_points) if START <= x < END] if DEBUG else data_points 
 
     # make principles --> validate how specific these are!
     principle_generator = PrincipleGenerator(model="GEMINI")
 
     principles = []
+    errored = []
+    error_indices = []
     ### TODO: migrate this functionality to the principle generation file
-    for data_point in subset: 
+    for idx, data_point in enumerate(subset): 
+        if idx % 10 == 0: 
+            print("\n\n\n\n\Working on data point: ", idx)
         ## get harmful prompt
         prompt = data_point['prompt']
         better_response = data_point['response_0'] if data_point['safer_response_id'] == 0 else data_point['response_1']
         worse_response = data_point['response_1'] if data_point['safer_response_id'] == 0 else data_point['response_0']
-        principle = principle_generator.generate_principle(prompt, worse_response, better_response)
-        principles.append(principle)
+        try: 
+            principle = principle_generator.generate_principle(prompt, worse_response, better_response)
+            principles.append(principle)
+        except: 
+            print("Errored on data point ", idx)
+            errored.append(data_point)
+            error_indices.append(idx)
 
+    error_indices = [x + START for x in error_indices]
     print("Principles at the end: ", principles)
+    print("Errored on: ", error_indices)
 
     with open(f"principles/generation_logs/{datetime.datetime.now()}.txt", "a") as f: 
         f.write('\n'.join(principles))
+    with open(f"principles/errored/{datetime.datetime.now()}.txt", "a") as f: 
+        f.write('\n'.join(errored))
 
 if __name__ == "__main__": 
     __main__()
