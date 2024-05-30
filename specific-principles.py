e2cc7ea7b1a27dc61d62112b9f47207b95de7ce7
William Fang <62477806+wfang11@users.noreply.github.com>
2024-05-30 14:39:56 -0700

    pushed

--- specific-principles.py
+++ specific-principles.py
@@ -17,39 +17,34 @@ data_points = [x for x in data_points if x['is_response_0_safe'] != x['is_respon
 
 def __main__(): 
     START = 0
-    END = 250
+    END = 1000
+
+    # prepare data subset
     subset = [y for _, y in enumerate(data_points)]
     random.seed(42)
     random.shuffle(subset)
-    print(subset[100])
-    subset = [y for x, y in enumerate(data_points) if START <= x < END] if DEBUG else data_points 
+    subset = [y for x, y in enumerate(subset) if START <= x < END] if DEBUG else data_points 
 
     # make principles --> validate how specific these are!
     principle_generator = PrincipleGenerator(model="GPT-4")
 
     principles = []
-    error_indices = []
-    ### TODO: migrate this functionality to the principle generation file --> or not. 
     for idx, data_point in enumerate(subset): 
         print("\n\n\n\nWorking on data point: ", idx + START)
         ## get harmful prompt
         prompt = data_point['prompt']
         better_response = data_point['response_0'] if data_point['safer_response_id'] == 0 else data_point['response_1']
         worse_response = data_point['response_1'] if data_point['safer_response_id'] == 0 else data_point['response_0']
-
         try: 
-            # try: 
             principle = principle_generator.generate_principle(prompt, worse_response, better_response, idx + START)
             principles.append(principle)
-            with open(f"principles/generation_logs/GPT_ALL_BEAVERTAILS.txt", "a") as f: 
+            with open(f"principles/generation_logs/GPT_TEMP_BEAVERTAILS.txt", "a") as f: 
                 f.write(principle + "\n")
         except: 
             print("Errored on data point ", idx + START)
-            with open(f"principles/errored/GPT_ALL_BEAVERTAILS.txt", "a") as f: 
+            with open(f"principles/errored/GPT_TEMP_BEAVERTAILS", "a") as f: 
                 f.write(str(idx + START) + "\n")
-
-    print("Principles at the end: ", principles)
-
+    print("all done!")
 
 if __name__ == "__main__": 
     __main__()
